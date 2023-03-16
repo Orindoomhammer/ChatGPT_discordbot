ee88792977fddd84b8f19056d4d302b1019ac2bd
Orindoomhammer <waco_opticon@yahoo.com>
2023-03-16 14:03:52 -0600

    Im getting a head ache with all these refactors!

--- ODH_Bot.py
+++ ODH_Bot.py
@@ -5,10 +5,6 @@ import subprocess
 import re
 from discord.ext import commands
 from dotenv import load_dotenv
-from discord.ext import tasks
-
-global bot_active
-
 
 def install_missing_packages():
     try:
@@ -18,8 +14,6 @@ def install_missing_packages():
         exit(1)
 
 install_missing_packages()
-
-# Your bot code here
 
 load_dotenv()
 
@@ -33,41 +27,13 @@ intents.typing = False
 intents.presences = False
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="Echo", intents=intents)
+bot = commands.Bot(command_prefix=None, intents=intents, help_command=None)
 openai.api_key = OPENAI_API_KEY
 
-@bot.command()
-async def V(ctx):
-    await ctx.send(f"Bot version: {bot_version} - Now with more awesomeness!")
-
-
-def should_reply(message):
-    # Add any conditions or keywords to check if the bot should reply to a message
-    keywords = ['AI', 'technology', 'chatbot', 'bot', 'Hello', 'anyone around?']
-    for keyword in keywords:
-        if keyword.lower() in message.content.lower():
-            return True
-    return False
-
 @bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
+async def on_ready():
+    print(f"We have logged in as {bot.user}")
 
-    if bot_active:
-        if should_reply(message):
-            prompt = f"User: {message.content}"
-            response = await get_chatgpt_response(prompt)
-            await message.channel.send(response)
-
-    await bot.process_commands(message)
-
-#@bot.command(name="ODH")
-#async def ask(ctx, *, question):
-#    response = await on_message(question)
-#    await ctx.send(response)
-
-@bot.command(name="ask")
 async def get_chatgpt_response(prompt, conversation_history=None):
     model_engine = "text-davinci-003"
     personality = (
@@ -95,46 +61,14 @@ async def get_chatgpt_response(prompt, conversation_history=None):
     message = response.choices[0].text.strip()
     return message
 
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
 
-@bot.command()
-async def code(ctx, *, request: str):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant that provides code snippets."},
-            {"role": "user", "content": request},
-        ],
-        max_tokens=100,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-
-    answer = response.choices[0].message['content']
-    code_snippet = re.findall(r"```[\s\S]*?```", answer)
-    if code_snippet:
-        await ctx.send(code_snippet[0])
-    else:
-        await ctx.send("I couldn't find a relevant code snippet for your request.")
-
-@bot.command(name="update_bot")
-async def update_bot(ctx):
-    if ctx.message.author.id == 472828401619697664:
-        if isinstance(ctx.channel, discord.DMChannel):
-            await ctx.send("Updating the bot, please wait...")
-            git_pull = subprocess.Popen("git pull", cwd=os.getcwd(), shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-            stdout, stderr = git_pull.communicate()
-            os._exit(1)
-            await ctx.send(f"Update result:\n{stdout.decode('utf-8')}\n{stderr.decode('utf-8')}")
-            await ctx.send("Restarting the bot...")
-            await bot.close()
-        else:
-            await ctx.send("This command can only be used in a DM with the bot.")
-    else:
-        await ctx.send("You don't have permission to use this command.")
-
-
-
-# Rest of your bot code
+    if f"{bot.user.name}".lower() in message.content.lower():
+        prompt = f"User: {message.content}"
+        response = await get_chatgpt_response(prompt)
+        await message.channel.send(response)
 
 bot.run(TOKEN)
