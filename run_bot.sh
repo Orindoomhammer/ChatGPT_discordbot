1515828f2b3d63b1d3cc787bb817d88ef6f3fe53
Orindoomhammer <waco_opticon@yahoo.com>
2023-03-16 10:57:40 -0600

    Update to Update .Sh

---
+++ run_bot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while true; do
+    python3 ODH_Bot.py
+    exit_status=$?
+    if [ $exit_status -eq 0 ]; then
+        echo "Bot exited normally. Restarting..."
+    elif [ $exit_status -eq 1 ]; then
+        echo "Bot exited with update signal. Updating and restarting..."
+        git pull
+    else
+        echo "Bot exited with status $exit_status. Stopping."
+        break
+    fi
+done
