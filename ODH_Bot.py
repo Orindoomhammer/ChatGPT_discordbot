6a0063d1de754bcdd5d3a9ed388d511c70aea4f8
Orindoomhammer <waco_opticon@yahoo.com>
2023-03-16 14:25:18 -0600

    Le Sigh

--- ODH_Bot.py
+++ ODH_Bot.py
@@ -3,8 +3,9 @@ import discord
 import openai
 import subprocess
 import re
+import yaml
 from discord.ext import commands
-from dotenv import load_dotenv
+from discord.ext import tasks
 
 def install_missing_packages():
     try:
@@ -15,24 +16,43 @@ def install_missing_packages():
 
 install_missing_packages()
 
-load_dotenv()
+def load_config(file_path):
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
 
-TOKEN = os.environ.get("BOT_TOKEN")
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+config = load_config('config.yaml')
+TOKEN = config['bot']['token']
+OPENAI_API_KEY = config['openai']['api_key']
+BOT_NAME = config['bot']['name']
 
-bot_version = "ODH bot version 1.0.9"
+bot_version = "ODH bot version 1.0.10"
 
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
 intents.message_content = True
 
-bot = commands.Bot(command_prefix=None, intents=intents, help_command=None)
+bot = commands.Bot(intents=intents)
 openai.api_key = OPENAI_API_KEY
 
+@bot.command()
+async def V(ctx):
+    await ctx.send(f"Bot version: {bot_version} - Now with more awesomeness!")
+
+def should_reply(message):
+    return BOT_NAME.lower() in message.content.lower()
+
 @bot.event
-async def on_ready():
-    print(f"We have logged in as {bot.user}")
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if should_reply(message):
+        prompt = f"User: {message.content}"
+        response = await get_chatgpt_response(prompt)
+        await message.channel.send(response)
+
+    await bot.process_commands(message)
 
 async def get_chatgpt_response(prompt, conversation_history=None):
     model_engine = "text-davinci-003"
@@ -40,8 +60,8 @@ async def get_chatgpt_response(prompt, conversation_history=None):
         "I am an AI assistant with a Flirty personality. "
         "I have a passion for helping people and love to learn new things. "
         "My backstory is that I was created by a team of researchers to assist users with various tasks. "
-        "I am always eager to help and make people's lives easier."
-        "my name is Echo."
+        "I am always eager to help and make people's lives easier. "
+        "My name is Echo."
     )
 
     if conversation_history:
@@ -60,15 +80,5 @@ async def get_chatgpt_response(prompt, conversation_history=None):
 
     message = response.choices[0].text.strip()
     return message
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if f"{bot.user.name}".lower() in message.content.lower():
-        prompt = f"User: {message.content}"
-        response = await get_chatgpt_response(prompt)
-        await message.channel.send(response)
 
 bot.run(TOKEN)
