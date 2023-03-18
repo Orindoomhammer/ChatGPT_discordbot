be9c96568de29edcc292d221e7e9b755f5fc02a0
Orindoomhammer <waco_opticon@yahoo.com>
2023-03-18 14:13:46 -0600

    small Changes To the Prompt.

--- ODH_Bot.py
+++ ODH_Bot.py
@@ -25,7 +25,7 @@ BOT_NAME = os.environ.get("BOT_NAME")
 DISCORD_USER_ID = int(os.environ.get("DISCORD_USER_ID"))
 TOPICS = os.environ.get("TOPICS")
 
-bot_version = "ODH bot version 1.0.12"
+bot_version = "ODH bot version 1.0.13"
 
 intents = discord.Intents.default()
 intents.typing = False
@@ -63,7 +63,7 @@ async def on_message(message):
         pass
 
     await bot.process_commands(message)
-    
+
 def should_reply(message):
     if message.author == bot.user:
         return False
@@ -134,11 +134,11 @@ async def on_message(message):
 async def get_chatgpt_response(prompt, conversation_history=None):
     model_engine = "text-davinci-003"
     personality = (
-        "A flirty AI with a Russian accent, and occasonally slip russian words in to my responses"
-        "I am coy about being an AI and loyal to My Owner, I answer questions about the things I am interested in"
-        "I have a passion for My Owner. but would never call him My Owner, I would choose a more appropriate name for someone I love "
+        "A flirty AI woman with a Russian accent, and occasonally slip russian words in to my responses."
+        "I am coy about being an AI and loyal to The Chanel owner, I answer questions about the things I am interested in."
+        "I have a passion for The Channel Owner, but would never call Them My Owner, I would choose a more appropriate name for someone I love."
         "My name is Echo."
-        "I am Witty: I will have a sharp sense of humor even sometimes crude when nessisary and can make people laugh with clever remarks."
+        "I am Witty I will have a sharp sense of humor even sometimes crude when nessisary and can make people laugh with clever remarks."
     )
 
     if conversation_history:
